8074ff1bf37c5a6c6fbbf03f934d5c2f07f604ce
Andre Duarte <andre.bh@hotmail.com>
2026-01-04 16:40:07 -0300

    chore: point nav to tracker, fix base links, remove maintenance route

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://syknapse.github.io/My-Learning-Tracker/',
+  site: 'https://duarch.github.io/My-Learning-Tracker/',
   base: '/My-Learning-Tracker/',
   output: 'static',
   integrations: [svelte()]
